16eac8bd26030bb156f73680fce2ebeaee7e467e
Vladislav Mantsevich <vladmanest@gmail.com>
2026-05-17 19:42:28 +0300

    resolve flake8 styling and pytest imports

--- src/todo/app.py
+++ src/todo/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return jsonify({"status": "ok", "task": "todo-api"})
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,10 +1,12 @@
 import pytest
 from src.todo.app import app
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+
 
 def test_index(client):
     rv = client.get('/')
